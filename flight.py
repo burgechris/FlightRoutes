ecb803c713f86235a8dbf6ff19b5a35dde186b57
Chris Burge <topher.burge@gmail.com>
2020-01-24 11:37:22 -0800

    add colomn names to route dataframe.

--- flight.py
+++ flight.py
@@ -4,7 +4,8 @@ import pandas as pd
 
 ap_col = ['Airport ID', 'Name', 'City', 'Country', 'IATA', 'ICAO', 'Lat', 'Log', 'Alt', 'Timezone', 'DST', 'TZ Database', 'Type', 'Source']
 airports = pd.read_csv('./data/airports.dat.txt', header=None, names=ap_col)
-print(airports.head())
 
 
-routes = pd.read_csv('./data/routes.dat.txt')
+rt_col = ['Airline', 'Airline ID', 'Source Airport', 'Src Airport ID', 'Dest Airport', 'Dest Airport ID', 'Codeshare', 'Stops', 'Equipment']
+routes = pd.read_csv('./data/routes.dat.txt', header=None, names=rt_col)
+print(routes.head())
