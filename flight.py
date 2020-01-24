966e84d2f2bb2acc3ae97744c888d8931364ff91
Chris Burge <topher.burge@gmail.com>
2020-01-24 11:33:09 -0800

    add column names to df and remove header.

--- flight.py
+++ flight.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-airports = pd.read_csv('./data/airports.dat.txt')
-
-airport = pd.read_csv('./data/routes.dat.txt')
+ap_col = ['Airport ID', 'Name', 'City', 'Country', 'IATA', 'ICAO', 'Lat', 'Log', 'Alt', 'Timezone', 'DST', 'TZ Database', 'Type', 'Source']
+airports = pd.read_csv('./data/airports.dat.txt', header=None, names=ap_col)
 print(airports.head())
+
+
+routes = pd.read_csv('./data/routes.dat.txt')
